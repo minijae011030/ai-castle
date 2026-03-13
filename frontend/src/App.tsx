6be5a661c8ec19d91997f8774fc109aaf637d620
Ian <105128049+minijae011030@users.noreply.github.com>
2026-03-13 20:00:40 +0900

    chore(front): tanstack query setting

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,15 @@
 import { RouterProvider } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 import { router } from './router'
+import { queryClient } from './lib/query-provider'
+import { QueryClientProvider } from '@tanstack/react-query'
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
       <TanStackRouterDevtools router={router} />
-    </>
+    </QueryClientProvider>
   )
 }
 
